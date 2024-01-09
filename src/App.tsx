c2263980bbf80852ea502dfb8730b1d79bc148dd
Bill Garrett <garrett@astralfrontier.org>
2024-01-09 05:52:57 -0800

    #8: implement a basic splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,41 @@ import { SceneIdentifier } from './scene-engine'
 
 import sceneTree from './scene-tree'
 
+enum GameMode {
+  TITLE, // The title screen
+  SCENES, // Playing through game scenes
+}
+
 function App() {
+  const [gameMode, setGameMode] = useState<GameMode>(GameMode.TITLE)
   const [sceneIdentifier, setSceneIdentifier] =
     useState<SceneIdentifier>('default')
 
   const defaultScene = sceneTree(sceneIdentifier)
-  return (
-    <div className="container">
-      <SceneView scene={defaultScene} setSceneIdentifier={setSceneIdentifier} />
-    </div>
-  )
+
+  switch (gameMode) {
+    case GameMode.TITLE:
+      return (
+        <div className="container">
+          <h1 className="title">Menagerie VN Demo</h1>
+          <a
+            className="button is-primary"
+            onClick={() => setGameMode(GameMode.SCENES)}
+          >
+            Start Playing
+          </a>
+        </div>
+      )
+    case GameMode.SCENES:
+      return (
+        <div className="container">
+          <SceneView
+            scene={defaultScene}
+            setSceneIdentifier={setSceneIdentifier}
+          />
+        </div>
+      )
+  }
 }
 
 export default App
