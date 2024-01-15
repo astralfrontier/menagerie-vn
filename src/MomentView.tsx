81753589b9de8e65d6f36997d013a978e641f75f
Bill Garrett <garrett@astralfrontier.org>
2024-01-15 05:48:41 -0800

    #28: Use absolute positioning for MomentView

--- src/MomentView.tsx
+++ src/MomentView.tsx
@@ -1,8 +1,8 @@
-import { find } from 'ramda'
+import { map } from 'ramda'
 import ChoiceView from './ChoiceView'
 import SpriteView from './SpriteView'
 import TextView from './TextView'
-import { SceneMoment, SceneSprite, SceneSpritePosition } from './scene-engine'
+import { SceneMoment, SceneSpritePosition } from './scene-engine'
 
 import classes from './moment-view.module.css'
 
@@ -10,45 +10,38 @@ interface MomentViewProps {
   moment: SceneMoment
 }
 
-// Return the first sprite found in a given position
-function spritesOnSide(
-  moment: SceneMoment,
-  position: SceneSpritePosition
-): SceneSprite | undefined {
-  return find((sprite) => sprite.position == position, moment.sprites)
-}
-
-// TODO: delegate to a subcomponent based on moment type
 export default function MomentView(props: MomentViewProps) {
   const { moment } = props
-
-  const spriteLeft = spritesOnSide(moment, SceneSpritePosition.CHARACTER_LEFT)
-  const spriteRight = spritesOnSide(moment, SceneSpritePosition.CHARACTER_RIGHT)
 
   const style = moment.background
     ? { backgroundImage: `url(${moment.background})` }
     : {}
 
   return (
-    <div
-      className={`columns is-gapless is-centered ${classes.backgroundImage}`}
-      style={style}
-    >
-      <div className={`column is-4 ${classes.spriteContainer}`}>
-        <div className={classes.spriteWrapper}>
-          <SpriteView sprite={spriteLeft} />
-        </div>
-      </div>
-      <div className="column is-4">
+    <div className={classes.backgroundImage} style={style}>
+      {map((sprite) => {
+        let characteralignment = ''
+        switch (sprite.position) {
+          case SceneSpritePosition.CHARACTER_LEFT:
+            characteralignment = 'characterleft'
+            break
+          case SceneSpritePosition.CHARACTER_RIGHT:
+            characteralignment = 'characterright'
+            break
+        }
+        return (
+          <div
+            className={`${classes.character} ${classes[characteralignment]}`}
+          >
+            <SpriteView sprite={sprite} />
+          </div>
+        )
+      }, moment.sprites)}
+      <div className={classes.text}>
         <div className={classes.centered}>
           <TextView text={moment.text} />
         </div>
         <ChoiceView choices={moment.choices} />
-      </div>
-      <div className={`column is-4 ${classes.spriteContainer}`}>
-        <div className={classes.spriteWrapper}>
-          <SpriteView sprite={spriteRight} />
-        </div>
       </div>
     </div>
   )
